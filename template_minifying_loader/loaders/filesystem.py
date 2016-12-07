ad9b5d2fdc4aedc7bfa9f9ab5ddb18294d6b05db
Lorand Arvay <lorand@sectorlabs.ro>
2016-12-07 15:56:25 +0200

    tests and setup

---
+++ template_minifying_loader/loaders/filesystem.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from django.template.loaders.filesystem import Loader as FilesystemLoader
+from django.template import Template
+
+from ..utils import get_template_minifier_strip_function
+
+class Loader(FilesystemLoader):
+    def get_template(self, template_name, template_dirs=None, skip=None):
+        template = super().get_template(template_name, template_dirs)
+
+        template_source = template.source
+        if getattr(settings, 'TEMPLATE_MINIFIER', True):
+            template_source = get_template_minifier_strip_function()(template.source)
+
+        return Template(
+            template_source,
+            template.origin,
+            template.name,
+            template.engine
+        )
---
+++ template_minifying_loader/tests/test_stripping.py
@@ -0,0 +1,35 @@
+from django.test import SimpleTestCase
+
+from ..utils import strip_spaces_in_template
+
+class TemplateStrippingTestCase(SimpleTestCase):
+
+
+    def test_html_stripping(self):
+        source = """<html>
+                <header><title>This is title</title></header>
+                <body>
+                Hello world
+                </body>
+                </html>"""
+        result = strip_spaces_in_template(source)
+        expected = """<html><header><title>This is title</title></header><body>
+                Hello world
+                </body></html>"""
+        self.assertEqual(result, expected)
+
+    def test_template_stripping(self):
+        source = """{% if latest_question_list %}
+                        <ul>
+                        {% for question in latest_question_list %}
+                            <li><a href="/polls/{{ question.id }}/">{{ question.question_text }}</a></li>
+                        {% endfor %}
+                        </ul>
+                    {% else %}
+                        <p>No polls are available.</p>
+                    {% endif %}"""
+        result = strip_spaces_in_template(source)
+        expected = '{% if latest_question_list %} <ul> {% for question in latest_question_list %} '\
+                   '<li><a href="/polls/{{ question.id }}/">{{ question.question_text }}</a></li> {% endfor %} '\
+                   '</ul> {% else %} <p>No polls are available.</p> {% endif %}'
+        self.assertEqual(result, expected)
