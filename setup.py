4c1c6b51e03aca28c468930e7a712540134343a5
Lorand Arvay <lorand@sectorlabs.ro>
2016-12-07 16:55:36 +0200

    setup.py added

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+setup(
+    name='django-template-minifying-loader',
+    version='1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',
+    description='Simple Django template loader that minifies html output.',
+    long_description=README,
+    url='https://github.com/SectorLabs/django-template-minifying-loader',
+    author='Sector Labs',
+    author_email='open-source@sectorlabs.ro',
+    keywords=['django', 'template', 'minification', 'html', 'minifier'],
+    packages=[
+        'template_minifying_loader',
+        'template_minifying_loader.loaders',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ]
+)
